515e2e71e3b56720f8b7d7febee1099e3bf8ac85
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-10 20:22:00 +0500

    Setup views

--- ColoredView/ViewController.swift
+++ ColoredView/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IBOutlets
+    @IBOutlet var coloredView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupElements()
     }
 
-
+    //MARK: - IBActions
+    @IBAction func sliderAction(_ sender: UISlider) {
+        switch sender.tag {
+        case 0:
+            redValueLabel.text = String(format: "%.2f", sender.value)
+        case 1:
+            greenValueLabel.text = String(format: "%.2f", sender.value)
+        default:
+            blueValueLabel.text = String(format: "%.2f", sender.value)
+        }
+        
+    }
+    
+    //MARK: - Private methods
+    private func setupElements() {
+        coloredView.layer.cornerRadius = 15
+        
+        redSlider.minimumTrackTintColor = .red
+        redSlider.maximumTrackTintColor = .red.withAlphaComponent(0.2)
+        
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.maximumTrackTintColor = .green.withAlphaComponent(0.2)
+        
+        blueSlider.minimumTrackTintColor = .blue
+        blueSlider.maximumTrackTintColor = .blue.withAlphaComponent(0.2)
+    }
 }
 
