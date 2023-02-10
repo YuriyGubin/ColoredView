9504a9a05f23bc95bde0a52cea552f62b69ffb34
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-10 20:33:20 +0500

    App is work

--- ColoredView/ViewController.swift
+++ ColoredView/ViewController.swift
@@ -20,9 +20,18 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    //MARK: - Properties
+    private var redComponent: Float = 0.5
+    private var greenComponent: Float = 0.5
+    private var blueComponent: Float = 0.5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupElements()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        coloredView.backgroundColor = UIColor(red: CGFloat(redComponent), green: CGFloat(greenComponent), blue: CGFloat(blueComponent), alpha: 1)
     }
 
     //MARK: - IBActions
@@ -30,12 +39,14 @@ class ViewController: UIViewController {
         switch sender.tag {
         case 0:
             redValueLabel.text = String(format: "%.2f", sender.value)
+            redComponent = sender.value
         case 1:
             greenValueLabel.text = String(format: "%.2f", sender.value)
+            greenComponent = sender.value
         default:
             blueValueLabel.text = String(format: "%.2f", sender.value)
+            blueComponent = sender.value
         }
-        
     }
     
     //MARK: - Private methods
