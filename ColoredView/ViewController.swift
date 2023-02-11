84b3de555c39258fde8c78f0f3aac149878fc99d
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-11 11:16:26 +0500

    App is ready

--- ColoredView/ViewController.swift
+++ ColoredView/ViewController.swift
@@ -23,27 +23,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupElements()
-    }
-    
-    override func viewWillLayoutSubviews() {
-        coloredView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )
+        setValue()
+        setColor()
     }
 
     //MARK: - IBActions
     @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
         
-        switch sender.tag {
-        case 0:
-            redValueLabel.text = String(format: "%.2f", sender.value)
-        case 1:
-            greenValueLabel.text = String(format: "%.2f", sender.value)
+        switch sender{
+        case redSlider:
+            redValueLabel.text = string(from: sender)
+        case greenSlider:
+            greenValueLabel.text = string(from: sender)
         default:
-            blueValueLabel.text = String(format: "%.2f", sender.value)
+            blueValueLabel.text = string(from: sender)
         }
     }
     
@@ -59,6 +53,25 @@ class ViewController: UIViewController {
         
         blueSlider.minimumTrackTintColor = .blue
         blueSlider.maximumTrackTintColor = .blue.withAlphaComponent(0.2)
+    }
+    
+    private func setColor() {
+        coloredView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue() {
+        redValueLabel.text = string(from: redSlider)
+        greenValueLabel.text = string(from: greenSlider)
+        blueValueLabel.text = string(from: blueSlider)
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
 }
 
